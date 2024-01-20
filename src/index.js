fd88bf102e7810eb70f9a813ba727d70dda57a90
LucasCruzZ4 <lucas.cruzdeoliver@gmail.com>
2024-01-20 16:53:53 -0300

    minha alteração no desafio deixando mais limpo

--- src/index.js
+++ src/index.js
@@ -1,30 +1,38 @@
-let nome;
-let exp;
-let novamente;
+// Desafio 1 Classificador de Herói
+console.log("Bem-vindo (a)")
 
-do {
-  nome = prompt("Digite o nome do personagem:");
-  exp = parseFloat(prompt("Digite a experiência:"));
+// Variáveis
+let nomeHeroi = "Reyna"
+let xpHeroi = 10000
+let nivel
 
-  if (exp < 1000) {
-    console.log("o personagem" +nome + "Você é Ferro");
-  } else if (exp >= 1000 && exp <= 2000) {
-    console.log("o personagem" +nome + "Você é Bronze");
-  } else if (exp > 2000 && exp <= 5000) {
-    console.log("o personagem" +nome + "Você é Prata");
-  } else if (exp > 5000 && exp <= 7000) {
-    console.log("o personagem" +nome  + "Você é Ouro");
-  } else if (exp > 7000 && exp <= 8000) {
-    console.log("o personagem" +nome + "Você é Platina");
-  } else if (exp > 8000 && exp <= 9000) {
-    console.log("o personagem" +nome + "Você é Ascendente");
-  } else if (exp > 9000 && exp <= 10000) {
-    console.log("o personagem" +nome + "Você é Imortal");
-  } else if (exp > 10000) {
-    console.log("o personagem" +nome + " é Radiante");
-  } else {
-    console.log("Tente novamente!");
-  }
+// Lógica
 
-  novamente = prompt(' Quer saber de outro personagem? (y ou n)');
-} while (novamente.toLowerCase() === 'y');
+switch (true) {
+    case xpHeroi <= 1000:
+        nivel = "Ferro"
+        break
+    case xpHeroi >= 1001 && xpHeroi <= 2000:
+        nivel = "Bronze"
+        break
+    case xpHeroi >= 2001 && xpHeroi <= 5000:
+        nivel = "Prata"
+        break
+    case xpHeroi >= 6001 && xpHeroi <= 7000:
+        nivel = "Ouro"
+        break
+    case xpHeroi >= 7001 && xpHeroi <= 8000:
+        nivel = "Platina"
+        break
+    case xpHeroi >= 8001 && xpHeroi <= 9000:
+        nivel = "Ascendente"
+        break
+    case xpHeroi >= 9001 && xpHeroi <= 10000:
+        nivel = "Imortal"
+        break
+    default:
+        nivel = "Radiante"
+        break
+}
+
+console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel)
